1068705fd4315e700e2f86aab3866a82a538ad1c
Calum Wicks <calumwicks@gmail.com>
2025-04-25 19:12:41 +1200

    Initial commit with backend and frontend setup

---
+++ frontend/components/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react'
+import TaskList from './TaskList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Todo List</h1>
+      <TaskList />
+    </div>
+  )
+}
+
+export default App
